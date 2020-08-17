a416028d103436fddd96dc45b284585b9278a555
Liad Keller <liadkeller@gmail.com>
2020-08-17 22:31:06 +0300

    Remove redundant attribute from MonthSchedule

--- month_schedule.py
+++ month_schedule.py
@@ -4,10 +4,8 @@ from csv_loadable import CSVLoadable
 class MonthSchedule(CSVLoadable):
     def __init__(self, month, year):
         self.dates_parser = DateParser(month, year)
-        self._csv_path = 'data/month.csv'
-
-        self._passenger_to_dates_map = {}
         self._date_to_passengers_map = {date: [] for date in self.dates_parser.all_dates()}
+        self._csv_path = 'data/month.csv'
 
         self.load()
     
@@ -21,7 +19,6 @@ class MonthSchedule(CSVLoadable):
     def add(self, name, dates_request):
         dates = self.parse_dates(dates_request)
 
-        self._passenger_to_dates_map[name] = dates
         for date in dates:
             self._date_to_passengers_map[date].append(name)
     
